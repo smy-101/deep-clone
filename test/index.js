7d642cd17705606a25a5fc6bc46cd0595f638345
smy-101 <824860484@qq.com>
2022-09-01 16:21:46 +0800

    初步完成

---
+++ test/index.js
@@ -0,0 +1,68 @@
+const chai = require("chai");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+const assert = chai.assert;
+const deepClone = require("../src/index");
+
+describe("deepClone", () => {
+  it("是一个函数", () => {
+    assert.isFunction(deepClone);
+  });
+  it("能够复制基本类型", () => {
+    const n = 123;
+    const n2 = deepClone(n);
+    assert(n === n2);
+    const s = "123456";
+    const s2 = deepClone(s);
+    assert(s === s2);
+    const b = true;
+    const b2 = deepClone(true);
+    assert(b === b2);
+    const u = undefined;
+    const u2 = deepClone(u);
+    assert(u === u2);
+    const empty = null;
+    const empty2 = deepClone(empty);
+    assert(empty === empty2);
+    const sym = Symbol();
+    const sym2 = deepClone(sym);
+    assert(sym === sym2);
+  });
+  describe("对象", () => {
+    it("能够复制普通对象", () => {
+      const a = { name: "xxx", child: { age: 18 } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.name === a2.name);
+      assert(a.child !== a2.child);
+      assert(a.child.age === a2.child.age);
+    });
+    it("能够复制数组对象", () => {
+      const a = [
+        [11, 12],
+        [21, 22],
+        [31, 32],
+      ];
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a[0] !== a2[0]);
+      assert(a[1] !== a2[1]);
+      assert(a[2] !== a2[2]);
+      assert.deepEqual(a, a2);
+    });
+    it("能够复制函数", () => {
+      const a = function (x, y) {
+        return x + y;
+      };
+      a.xxx = { yyy: { zzz: 1 } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+      assert(a.xxx !== a2.xxx);
+      assert(a.xxx.yyy !== a2.xxx.yyy);
+      assert(a(1, 2) === a2(1, 2));
+    });
+  });
+});
